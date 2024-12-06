8ac257de0afa6ac7c66e111b82fe14ce372b01f4
Henry Enoch <zs22017009@estudiantes.uv.mx>
2024-12-06 10:08:31 -0600

    CRUD EducationalExperience api

--- app/Http/Controllers/EducationalExperienceController.php
+++ app/Http/Controllers/EducationalExperienceController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\EducationalExperience;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class EducationalExperienceController extends Controller
 {
@@ -18,11 +20,11 @@ class EducationalExperienceController extends Controller
     }
 
     /**
+     * //
      * Show the form for creating a new resource.
      */
     public function create()
     {
-        //
     }
 
     /**
@@ -30,7 +32,28 @@ class EducationalExperienceController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'maestroID' => 'required|numeric',
+            'nrc' => 'required|string|max:5',
+            'nombre' => 'required|string',
+            'modalidad' => 'required|string',
+            'descripcion' => 'nullable|string'
+        ]);
+
+        $educationalExperience = EducationalExperience::create([
+            'teacher_id' => $request->maestroID,
+            'nrc' => $request->nrc,
+            'name' => $request->nombre,
+            'modality' => $request->modalidad,
+            'description' => $request->descripcion,
+            'created_by' => Auth::id(),
+            'updated_by' => Auth::id(),
+        ]);
+
+        return response()->json([
+           'message' => "Experiencia educativa creada exitosamente",
+            'data' => $educationalExperience,
+        ], 201);
     }
 
     /**
@@ -46,22 +69,51 @@ class EducationalExperienceController extends Controller
      */
     public function edit(string $id)
     {
-        //
+
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $educationalExperience)
     {
-        //
+        $educationalExp = EducationalExperience::findOrFail($educationalExperience);
+        $request->validate([
+            'maestroID' => 'required|numeric',
+            'nrc' => 'required|string|max:5',
+            'nombre' => 'required|string',
+            'modalidad' => 'required|string',
+            'descripcion' => 'nullable|string'
+        ]);
+
+        $educationalExp->update([
+            'teacher_id' => $request->maestroID,
+            'nrc' => $request->nrc,
+            'name' => $request->nombre,
+            'modality' => $request->modalidad,
+            'description' => $request->descripcion,
+            'updated_at' => Carbon::now(),
+            'created_by' => Auth::id(),
+            'updated_by' => Auth::id(),
+        ]);
+
+        return response()->json([
+            'message' => "Experiencia editada exitosamente",
+            'data' => $educationalExp,
+        ], 201);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy($educationalExperience)
     {
-        //
+        $educationalExp = EducationalExperience::findOrFail($educationalExperience);
+        $educationalExp->delete();
+
+        return response()->json([
+            'message' => 'Experiencia educativa eliminada exitosamente'
+        ], 201);
+
     }
 }
