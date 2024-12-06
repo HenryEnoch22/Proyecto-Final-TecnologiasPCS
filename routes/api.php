52dd9886931d5b666db34e4385052cad932e671e
Henry Enoch <zs22017009@estudiantes.uv.mx>
2024-12-06 08:19:33 -0600

    User and Role Seeder | EE Controller and Model

---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\EducationalExperienceController;
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+    Route::resource('experiencia-educativa', EducationalExperienceController::class);
+});*/
+
+Route::middleware('auth:sanctum')->group(function (){
+
+});
+Route::resource('experiencia-educativa', EducationalExperienceController::class);
+
+
