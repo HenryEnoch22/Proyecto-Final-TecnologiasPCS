6a8f4eebaeaa9cf9b0394ab58eb426530084ddf3
Sebati HM <sebashermon003@gmail.com>
2024-12-07 00:42:21 -0600

    Group Controller avance 1

--- app/Http/Controllers/GroupControlller.php
+++ app/Http/Controllers/GroupControlller.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+use App\Models\Group;
 
 class GroupControlller extends Controller
 {
@@ -12,7 +14,8 @@ class GroupControlller extends Controller
      */
     public function index()
     {
-        //
+        $groups = Group::all();
+        return response()->json($groups, 200);
     }
 
     /**
@@ -20,7 +23,6 @@ class GroupControlller extends Controller
      */
     public function create()
     {
-        //
     }
 
     /**
@@ -28,7 +30,28 @@ class GroupControlller extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'id' => 'required|numeric'
+            ,'educational_experience_id' => 'required|numeric'
+            ,'student_id' => 'required|numeric'
+            ,'shift' => 'required|string|max:5'
+            ,'period' => 'required|string|max:45'
+            ,'student_grade' => 'nullable|numeric'
+        ]);
+
+        $group = Group::create([
+            'id' => $request->id
+            ,'educational_experience_id' => $request->educational_experience_id
+            ,'student_id' => $request->student_id
+            ,'shift' => $request->shift
+            ,'period' => $request->period
+            ,'student_grade' => $request->student_grade
+        ]);
+
+        return response()->json([
+            'message' => 'Alumno registrado en el grupo exitosamente'
+            ,'data' => $group
+        ],201);
     }
 
     /**
@@ -50,16 +73,63 @@ class GroupControlller extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $groupRequested)
     {
-        //
+        $group = Group::findOrFail($groupRequested);
+        $request->validate([
+            'id' => 'required|numeric'
+            ,'educational_experience_id' => 'required|numeric'
+            ,'student_id' => 'required|numeric'
+            ,'shift' => 'required|string|max:5'
+            ,'period' => 'required|string|max:45'
+            ,'student_grade' => 'nullable|numeric'
+        ]);
+
+        $group->update([
+            'id' => $request->id
+            ,'educational_experience_id' => $request->educational_experience_id
+            ,'student_id' => $request->student_id
+            ,'shift' => $request->shift
+            ,'period' => $request->period
+            ,'student_grade' => $request->student_grade
+        ]);
+
+
+        return response()->json([
+            'message' => 'Se ha editado el registro del alumno en el curso'
+            ,'data' => $group
+        ],201);
+
+
+        
+    }
+
+    public function assignGrade(Request $grade, $groupRequested){
+        $group = Group::findOrFail($groupRequested);
+        $grade->validate([
+            'grade' => 'required|numeric'
+        ]);
+
+        $group->update(['student_grade' => $grade->grade]);
+
+        return response()->json([
+            'message' => 'Se ha asignado la calificacion al alumno elegido'
+            ,'data' => $group
+        ],201);
+
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy($groupRequested)
     {
-        //
+        $group = Group::findOfFail($groupRequested);
+        $group->delete();
+
+        return response()->json([
+            'message' => 'Se ha dado de baja al alumno del grupo'
+            ,'data' => $group
+        ],201);
     }
 }
--- app/Models/Group.php
+++ app/Models/Group.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Group extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    public $timestamps = true;
+    protected $table = 'group';
+    public $fillable = ['id','educational_experience_id','student_id','shift','period','student_grade'];
+    
 }
